182ada98776ce03a3c40a6e2440e49c3869f5b7f
Hansenyao <yyfsingler@163.com>
2025-08-08 00:18:09 -0600

    remove unuseful code

--- dmit-1530-assignment-2/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.getElementById("hamburger-menu");
-    const navLinks = document.getElementById("hamburger-menu-links");
-
-    /* Show/Hide hamburger menu navigation */
-    hamburger.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
-    });
-});
